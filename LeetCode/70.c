685ea2f9b03286f77c49fbc8b4bdb761df30f172
Cupidlyh <48281017+Cupidlyh@users.noreply.github.com>
2019-07-25 19:49:22 +0800

    C语言

---
+++ LeetCode/70.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int climbStairs(int n){
+    int arr[10000]={0};
+    arr[1]=1;
+    arr[2]=2;
+    int i;
+    for(i=3;i<=n;i++)
+        arr[i]=arr[i-1]+arr[i-2];
+    return arr[n];
+}
