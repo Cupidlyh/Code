f79e8acaf705d3bbbdf7dfca1dbf5b2965705f43
Cupidlyh <48281017+Cupidlyh@users.noreply.github.com>
2019-08-18 19:58:48 +0800

    C语言

---
+++ LeetCode/48.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void rotate(int** matrix, int matrixSize, int* matrixColSize){
+    int i,j,temp,n=matrixSize;
+    for(i=0;i<n;i++){
+        for(j=i;j<n-i-1;j++){//½»»»ÔªËØ
+            temp=matrix[i][j];
+            matrix[i][j]=matrix[n-j-1][i];
+            matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
+            matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
+            matrix[j][n-i-1]=temp;
+        }
+    }
+}
