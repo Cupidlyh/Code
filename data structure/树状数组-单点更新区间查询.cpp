a1dba4bb5658af4c85eb18ac878d3395dfe74cb5
Cupidlyh <48281017+Cupidlyh@users.noreply.github.com>
2020-03-18 22:13:20 +0800

    C++

---
+++ data structure/树状数组-单点更新区间查询.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<algorithm>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+int a[50005],c[50005],N;//对应原数组和树状数组
+
+int lowbit(int x)
+{
+    return x&(-x);
+}
+
+int getsum(int i)//求a[1]到a[i]的和
+{
+    int res = 0;
+    while(i > 0)
+    {
+        res += c[i];
+        i -= lowbit(i);
+    }
+    return res;
+}
+
+void updata(int i,int k)//在i位置加上k（此函数只更改树状数组的值，若要更改原数组的值需要在主函数中实现）
+{
+    while(i <= N)
+    {
+        c[i] += k;
+        i += lowbit(i);
+    }
+}
+
+int main()
+{
+    cin>>N;//N为数组中元素个数
+    for(int i = 1; i <= N; i++)//注意一定要从原始数组下标大于等于1的位置开始存储
+    {
+        cin>>a[i];
+        updata(i,a[i]);//输入初值的时候，也相当于更新了值
+    }
+    return 0;
+}
