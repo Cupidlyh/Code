36822179307606ae11d65a6ac1d8ddb83b5aa2c2
Cupidlyh <48281017+Cupidlyh@users.noreply.github.com>
2020-07-24 12:17:29 +0800

    C++

---
+++ LeetCode/154.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int mid;
+        int left = 0,right = nums.size() - 1;
+        //¶þ·Ö²éÕÒ
+        while(left < right) {
+            mid = left + (right - left) / 2;
+            if(nums[right] > nums[mid]) {
+                right = mid;
+            }
+            else if(nums[right] < nums[mid]) {
+                left = mid + 1;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+};
